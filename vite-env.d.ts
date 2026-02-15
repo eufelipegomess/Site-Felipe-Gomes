e9a93df647ee9bd799443802a66224be67725365
eufelipegomess <137424277+eufelipegomess@users.noreply.github.com>
2026-02-15 12:31:22 -0300

    chore: Add env file ignore
    Add `.env` and related files to `.gitignore` to prevent sensitive environment variables from being committed. Also, comment out the unused `vite/client` reference in `vite-env.d.ts`.

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,9 +1,10 @@
-/// <reference types="vite/client" />
+// /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string
   readonly VITE_SUPABASE_ANON_KEY: string
   readonly VITE_GOOGLE_API_KEY: string
+  readonly [key: string]: string | boolean | undefined
 }
 
 interface ImportMeta {
